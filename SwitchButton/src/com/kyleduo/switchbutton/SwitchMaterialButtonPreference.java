d61c9baba68219a1b14f93bfafea8e7246fa09ac
Andrew Chen <yongjhih@gmail.com>
2014-11-20 01:42:22 +0800

    Indenpendent SwitchMaterialButtonPreference

--- SwitchButton/src/com/kyleduo/switchbutton/SwitchMaterialButtonPreference.java
+++ SwitchButton/src/com/kyleduo/switchbutton/SwitchMaterialButtonPreference.java
@@ -1,27 +1,31 @@
 package com.kyleduo.switchbutton;
 
 import android.content.Context;
+import android.preference.CheckBoxPreference;
 import android.util.AttributeSet;
 
-public class SwitchMaterialButtonPreference extends SwitchButtonPreference {
+public class SwitchMaterialButtonPreference extends CheckBoxPreference {
 
     public SwitchMaterialButtonPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public SwitchMaterialButtonPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
     }
 
     public SwitchMaterialButtonPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public SwitchMaterialButtonPreference(Context context) {
         super(context);
+        init();
     }
 
-    @Override
     protected void init() {
         setWidgetLayoutResource(R.layout.preference_switch_material_button_layout);
     }
